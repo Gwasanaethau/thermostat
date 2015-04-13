340f192ad732c209b349302f9d98436f38af0749
Mark Pokorny <iridium193@gmail.com>
2015-04-13 14:03:08 +0100

    Updated ‘JavaBuzzSpec.js’ with some actual tests!

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -1,3 +1,10 @@
 describe('JavaBuzz', function() {
+  var javabuzz;
 
+  describe('knows when a number is', function() {
+    it('divisible by three', function() {
+      javabuzz = new JavaBuzz();
+      expect(isDivisibleByThree(3)).toBe(true);
+    });
+  });
 });
